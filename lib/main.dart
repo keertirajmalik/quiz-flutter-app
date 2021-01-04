f266e65b46aa396748786bd75a369e84d8337ffc
keertirajmalik <keertirajmalik@gmail.com>
2021-01-04 20:44:57 +0530

    2 button for true and false and displaying the question in the center

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class Quizzler extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: EdgeInsets.all(10.0),
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
         child: QuizPage(),
       ),
     );
@@ -28,28 +28,65 @@ class QuizPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Center(
-          child: Text(
-            'Hello world',
-            style: TextStyle(color: Colors.white),
+        Expanded(
+          flex: 6,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where question go',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
           ),
         ),
-        RaisedButton(
-          color: Colors.red,
-          onPressed: () {
-            print('True');
-          },
-          child: null,
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              onPressed: () {},
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+          ),
         ),
-        RaisedButton(
-          color: Colors.green,
-          onPressed: () {
-            print('False');
-          },
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.red,
+              onPressed: () {},
+              child: Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+          ),
         ),
       ],
     );
   }
 }
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
