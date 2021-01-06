02194dfe0cbf28eedbe8ef8c8454a575cc72e1e1
keertirajmalik <keertirajmalik@gmail.com>
2021-01-06 22:45:48 +0530

    Create Question class which will be holding the question and answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/Question.dart';
 
 void main() {
   runApp(
@@ -31,13 +32,16 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
 
-  List<bool> answers = [false, true, true];
+  List<Question> questionBank = [
+    Question(
+        question: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        question: 'Approximately one quarter of human bones are in the feet',
+        answer: true),
+    Question(question: 'A slug\'s blood is green.', answer: true)
+  ];
 
   int questionNumber = 0;
 
@@ -52,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.white,
@@ -69,7 +73,7 @@ class _QuizPageState extends State<QuizPage> {
               textColor: Colors.white,
               color: Colors.green,
               onPressed: () {
-                var correctAnswer = answers[questionNumber];
+                var correctAnswer = questionBank[questionNumber].answer;
                 if (correctAnswer == true) {
                   setState(() {
                     scoreKeeper.add(
@@ -109,7 +113,7 @@ class _QuizPageState extends State<QuizPage> {
               textColor: Colors.white,
               color: Colors.red,
               onPressed: () {
-                var correctAnswer = answers[questionNumber];
+                var correctAnswer = questionBank[questionNumber].answer;
                 if (correctAnswer == false) {
                   setState(() {
                     scoreKeeper.add(
